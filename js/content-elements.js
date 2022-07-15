e637328021a7db8c01a972726cde4022c24ddc6c
twombly12 <amian.dimichele@gmail.com>
2022-07-15 00:37:49 -0400

    fix template page, turn menu items into variables and add to footer, add date variable to copyright

--- js/content-elements.js
+++ js/content-elements.js
@@ -1,7 +1,6 @@
 function head() {
     let head = document.querySelector('head')
-    head.innerHTML = `
-    <link rel="stylesheet" href="css/style.css">
+    head.innerHTML += `
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Yantramanav:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
@@ -10,22 +9,30 @@ function head() {
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     `
 }
+const menuLinks = {
+    'services': `
+            <li><a href="">Websites</a></li>
+            <li><a href="">Web Apps</a></li>
+            <li><a href="/logos.html">Logos</a></li>
+            <li><a href="">Testimonials</a></li>
+        `,
+
+    'info': `
+            <li><a class="active" href="">About</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Contact</a></li>
+                    `,
+}
 
 function header() {
     let header = document.querySelector('header')
     header.innerHTML = `
-<nav id="main-menu" class="menu">
+        <nav id="main-menu" class="menu">
             <div>
                 <img class="logo" src="assets/Dash Logo Colours-White.svg" href="#" alt="Dash Interactive Logo" />
                 <ul class="main-menu">
-                    <li><a class="active" href="">About</a></li>
-                    <li><a href="">Websites</a></li>
-                    <li><a href="">Web Apps</a></li>
-                    <li><a href="/logos.html">Logos</a></li>
-                    <li><a href="">Testimonials</a></li>
-                    <li><a href="">Blog</a></li>
-                    <li><a href="">Contact</a></li>
-
+                    ${menuLinks.services}
+                    ${menuLinks.info}
                     <label class="switch">
                         <input type="checkbox" value="on">
                         <span class="slider round"></span>
@@ -40,13 +47,8 @@ function header() {
                 <img class="logo" src="https://d33wubrfki0l68.cloudfront.net/f525bebad8ec45967899542ab37f9756287e1bbf/b31d2/images/logo-light-desktop.png" href="#" alt="Dash Interactive Logo" />
                 <span id="hamburger"><span id="toggle"></span></span>
                 <ul class="main-menu">
-                    <li><a class="active" href="">About</a></li>
-                    <li><a href="">Websites</a></li>
-                    <li><a href="">Web Apps</a></li>
-                    <li><a href="">Logos</a></li>
-                    <li><a href="">Testimonials</a></li>
-                    <li><a href="">Blog</a></li>
-                    <li><a href="">Contact</a></li>
+                    ${menuLinks.services}
+                    ${menuLinks.info}
 
                     <label class="switch">
                         <input type="checkbox" value="on">
@@ -87,19 +89,13 @@ function footer() {
                 <div>
                     <h3>Services</h3>
                     <ul>
-                        <li><a href="">Websites</a></li>
-                        <li><a href="">Web Apps</a></li>
-                        <li><a href="">Logos</a></li>
-                        <li><a href="">Testimonials</a></li>
+                    ${menuLinks.services}
                     </ul>
                 </div>
                 <div>
                     <h3>Info</h3>
                     <ul>
-                        <li><a href="">About</a></li>
-                        <li><a href="">Blog</a></li>
-                        <li><a href="">FAQ</a></li>
-                        <li><a href="">Contact</a></li>
+                        ${menuLinks.info}
                     </ul>
                 </div>
                 <div>
@@ -107,7 +103,7 @@ function footer() {
                 </div>
             </div>
             <div id="copyright">
-                <span>Copyright © 2021 | Dash Interactive | All Rights Reserved | Terms of Use | Privacy Policy</span>
+                <span>Copyright © ${new Date().getFullYear()} | Dash Interactive | All Rights Reserved | Terms of Use | Privacy Policy</span>
             </div>
             `
 }
